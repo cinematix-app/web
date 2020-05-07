707ce3a725a95fc1e7513ad2243ceb098d50bfb7
David Barratt <david@davidwbarratt.com>
2020-05-06 22:16:10 -0400

    Use new library

--- index.js
+++ index.js
@@ -1,72 +1,7 @@
-import { getAssetFromKV } from '@cloudflare/kv-asset-handler';
+import { getEventHandler } from '@cinematix/next-cloudflare';
+import routesManifest from './.next/routes-manifest.json';
 
-/**
- * The DEBUG flag will do two things that help during development:
- * 1. we will skip caching on the edge, which makes it easier to
- *    debug.
- * 2. we will return an error message on exception in your Response rather
- *    than the default 404.html page.
- */
-const DEBUG = false;
-
-async function handleEvent(event) {
-  const url = new URL(event.request.url);
-  const options = {};
-
-  // If this is in the /_next/ folder than it can effectively be
-  // cached forever since the next version will have new filenames.
-  if (url.pathname.match(/^\/_next\/.*$/)) {
-    options.cacheControl = {
-      edgeTTL: 31536000, // 1 year
-      browserTTL: 31556952, // 1 year
-    };
-  }
-
-  /**
-   * You can add custom logic to how we fetch your assets
-   * by configuring the function `mapRequestToAsset`
-   */
-  // options.mapRequestToAsset = handlePrefix(/^\/docs/)
-
-  try {
-    if (DEBUG) {
-      // customize caching
-      options.cacheControl = {
-        bypassCache: true,
-      };
-    }
-    return await getAssetFromKV(event, options);
-  } catch (e) {
-    // if an error is thrown try to serve the asset at 404.html
-    if (!DEBUG) {
-      try {
-        const notFoundResponse = await getAssetFromKV(event, {
-          mapRequestToAsset: req => new Request(`${new URL(req.url).origin}/404.html`, req),
-        });
-
-        return new Response(notFoundResponse.body, { ...notFoundResponse, status: 404 });
-      } catch (error) {
-        return new Response(error.message || error.toString(), { status: 500 });
-      }
-    }
-
-    return new Response(e.message || e.toString(), { status: 500 });
-  }
-}
+const handleEvent = getEventHandler(routesManifest);
 
 // eslint-disable-next-line no-restricted-globals
-addEventListener('fetch', (event) => {
-  try {
-    return event.respondWith(handleEvent(event));
-  } catch (e) {
-    if (DEBUG) {
-      return event.respondWith(
-        new Response(e.message || e.toString(), {
-          status: 500,
-        }),
-      );
-    }
-
-    return event.respondWith(new Response('Internal Error', { status: 500 }));
-  }
-});
+addEventListener('fetch', handleEvent);
